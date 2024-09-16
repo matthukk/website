7477ca9652687dc32ccc691b6fd234a202c868b0
matthukk <77340924+matthukk@users.noreply.github.com>
2024-09-16 12:56:38 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    colors: {
+      'wprimary': '#009407'
+    },
+    fontFamily: {
+      'league': ['League Spartan', 'Arial', 'sans-serif'],
+      'libre': ['Libre Baskervile', 'Arial', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
+
